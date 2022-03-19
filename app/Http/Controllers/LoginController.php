e01645f68f55fb44998dd324557361d2c6519ddd
Adriano Oliveira <adrianooliveira2332@gmail.com>
2022-03-19 18:42:19 -0300

    LoginController ready

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,12 +12,39 @@ class LoginController extends Controller
     public function index(Request $request){
         $erro = '';
         if($request->get('erro') == 1){
-
+            $erro = 'E-mail e/ou senha inválidos';
         }
         if($request->get('erro') == 2){
-
+            $erro = 'Necessário autenticação';
         }
 
         return view('login', ['erro' => $erro]);
+    }
+
+    public function authenticate(Request $request){
+        $rules = [
+            'email' => 'required|email',
+            'password' => 'required',
+        ];
+        $feedback = [
+            'required' => 'Você esqueceu de me preencher!',
+            'email' => 'Digite um email válido!'
+        ];
+        $request->validate($rules,$feedback);
+
+        $credentials = $request->only('email', 'password');
+        $remember = $request->only('remember');
+
+        if(Auth::attempt($credentials,$remember)){
+            return redirect()->intended('panel');
+        }
+
+        return redirect()->route('login',['erro' => 1]);
+    }
+
+
+    public function logout(){
+        auth::logout();
+        return redirect()->route('login');
     }
 }
