e03332b089cdf8aa71ec6c32dc62bd486581b56d
Adriano Oliveira <adrianooliveira2332@gmail.com>
2022-03-20 14:53:13 -0300

    new file css for pannel, add logo and page index-pannel done

--- resources/views/pannel/index.blade.php
+++ resources/views/pannel/index.blade.php
@@ -1,8 +1,10 @@
 @extends('adminlte::page')
 
 @section('content')
-    <main class="principal-painel">
-        <img class="logo-home-painel" src="{{asset('assets/images/Samir_Logo-02.svg')}}" alt="">
-        <p>Bem vindo ao Painel Administrativo do site Lab </p>
-    </main>
+    <section id="home">
+        <main class="main-pannel">
+            <img src="{{asset('assets/images/Logo-lab.png')}}" alt="logo da empresa Lab">
+            <p>Bem vindo ao Painel Administrativo do site Lab </p>
+        </main>
+    </section>
 @endsection
