090d87c9ec35188cff0e71bc93722171ed8ed91c
Adriano Oliveira <adrianooliveira2332@gmail.com>
2022-03-20 09:20:21 -0300

    route and view 'index'panel created

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/new-user', 'UserController@new')->name('new-user-get');
 Route::post('/new-user', 'UserController@save')->name('new-user-post');
 
 Route::middleware('login')->prefix('panel')->group(function(){
+    Route::get('/', function(){return view('panel');})->name('panel');
     
     Route::resource('/services', 'ServiceController');
     Route::resource('/blogs', 'BlogController');
