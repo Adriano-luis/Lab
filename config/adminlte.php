ffa7e2aca3545d62829d536e1cf86095d59b74c9
Adriano Oliveira <adrianooliveira2332@gmail.com>
2022-03-20 14:57:40 -0300

    logo add from all pannel adminLTE

--- config/adminlte.php
+++ config/adminlte.php
@@ -46,8 +46,8 @@ return [
     */
 
     'logo' => '<b>Lab</b>',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
-    'logo_img_class' => 'brand-image img-circle elevation-3',
+    'logo_img' => asset('assets/images/Logo-lab.png'),
+    'logo_img_class' => 'brand-image elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
     'logo_img_alt' => 'Lab',
