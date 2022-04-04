d63dab26fce72a40e3d3166cd77c3105e6f6aee5
Adriano Oliveira <adrianooliveira2332@gmail.com>
2022-04-04 13:10:52 -0300

    Site: first letter in header to uppercase

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,19 +2,19 @@
     <a href="{{route('home')}}"><img src="{{asset('assets/images/Logo-lab.webp')}}" alt="Logo do site Lab" title="Logo"></a>
     <nav id="menu">
         <ul>
-            <li><a class="pageScroll" href="#services" aria-label="services">serviços</a></li>
-            <li><a class="pageScroll" href="#about" aria-label="about">sobre</a></li>
-            <li><a class="pageScroll" href="#blog" aria-label="blog">blog</a></li>
-            <li><a href="{{route('contact')}}">contato</a></li>
+            <li><a class="pageScroll" href="#services" aria-label="services">Serviços</a></li>
+            <li><a class="pageScroll" href="#about" aria-label="about">Sobre</a></li>
+            <li><a class="pageScroll" href="#blog" aria-label="blog">Blog</a></li>
+            <li><a href="{{route('contact')}}">Contato</a></li>
         </ul>
     </nav>
     <img class="icon-mobile" src="{{asset('assets/images/menu.png')}}" alt="Icone de menu para mobile" title="Menu mobile" onclick="toggleMenu()">
     <nav id="mobile">
         <div id="submenu">
-            <a class="pageScroll" href="#services" aria-label="services">serviços</a>
-            <a class="pageScroll" href="#about" aria-label="about">sobre</a>
-            <a class="pageScroll" href="#blog" aria-label="blog">blog</a>
-            <a href="{{route('contact')}}">contato</a>
+            <a class="pageScroll" href="#services" aria-label="services">Serviços</a>
+            <a class="pageScroll" href="#about" aria-label="about">Sobre</a>
+            <a class="pageScroll" href="#blog" aria-label="blog">Blog</a>
+            <a href="{{route('contact')}}">Contato</a>
         </div>
     </nav>
 </header>
