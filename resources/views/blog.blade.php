8cf9fbd77388409fc9ef1154e99da0ed9271bb8b
Adriano Oliveira <adrianooliveira2332@gmail.com>
2022-04-06 18:07:11 -0300

    Site: validate for articles in page blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -20,12 +20,14 @@ Blog
         </aside>
     </div>
     <div class="blogs-main" id="blogs-main">
-        @foreach ($articles as $article)    
-            <article>
-                <h3>{{$article->title}}</h3>
-                <p>{{$article->description}}</p>
-                <a href="{{route('article',['article' => $article->id])}}">Continue Lendo</a>
-            </article>
+        @foreach ($articles as $article)  
+            @if($article->active == 1)
+                <article>
+                    <h3>{{$article->title}}</h3>
+                    <p>{{$article->description}}</p>
+                    <a href="{{route('article',['article' => $article->id])}}">Continue Lendo</a>
+                </article>
+            @endif  
         @endforeach
     </div>
 </section>
